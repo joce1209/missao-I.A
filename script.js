5014d3bc04f63e59d9fb050c4b1c9a8c4a85e853
Jocemara <130997343+joce1209@users.noreply.github.com>
2024-06-17 17:02:27 -0300

    alt

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const caixaPrincipal = document.querySelector ('.caixa-principal');
-    const caixapergunta = document.querySelector ('.caixa-pergunta' );
-    const caixaAlternativas = document.querySelector ('.caixa-alternativas' );
-  const caixaResultado = document.querySelector ('.caixa-resultados' );
-  const caixaResultado = document.querySelector ('.caixa-resultados' );
-    
- const perguntas =[//abre lista de perguntas
- {//abre objeto de perguntas
-     enunciado="pergunta 1",
-   alternativas=
-   [ alternativa 1",
-    "alternativa2"
-    ]   
- }//fecha objeto de perguntas 
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixapergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultados');
+const caixaResultado = document.querySelector('.caixa-resultados');
+
+const perguntas = [//abre lista de perguntas
+    {//abre objeto de perguntas
+        enunciado:"pergunta 1",
+        alternativas:[
+            alternativa 1",
+            "alternativa 2"
+        ]
+    }//fecha objeto de perguntas 
 ]   //fecha lista de perguntas
