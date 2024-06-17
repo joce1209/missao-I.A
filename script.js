a4dcb94b9fef03346e6578f544e7749b86911d50
Jocemara <130997343+joce1209@users.noreply.github.com>
2024-06-17 16:42:46 -0300

    query

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const caixaPrincipal = document.querySelector (´.caixa-principal` );
     const caixapergunta = document.querySelector (´.caixa-pergunta` );
     const caixaAlternativas = document.querySelector (´.caixa-alternativas` );
-        const caixaResultado = document.querySelector (´.caixa-` );
+  const caixaResultado = document.querySelector (´.caixa-resultados` );
+  const caixaResultado = document.querySelector (´.caixa-resultados` );
