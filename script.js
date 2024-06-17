0b26f591727ec8cd19b80129a4c0a903d0ba280f
Jocemara <130997343+joce1209@users.noreply.github.com>
2024-06-17 16:41:43 -0300

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document.querySelector (´.caixa-principal` );
+    const caixapergunta = document.querySelector (´.caixa-pergunta` );
+    const caixaAlternativas = document.querySelector (´.caixa-alternativas` );
+        const caixaResultado = document.querySelector (´.caixa-` );
